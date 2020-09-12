25e60987c7a74e8faed98688e443e98688c84adc
swaaz <swaasthik.shetty07@gmail.com>
2020-09-13 02:48:55 +0530

    header completed

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB18twOyl4gvpdGon9Wh2lI6GP075i-xaQ",
+    authDomain: "fir-b97e5.firebaseapp.com",
+    databaseURL: "https://fir-b97e5.firebaseio.com",
+    projectId: "fir-b97e5",
+    storageBucket: "fir-b97e5.appspot.com",
+    messagingSenderId: "256845023152",
+    appId: "1:256845023152:web:cfc1389901444ce71ff875",
+    measurementId: "G-SGPPGN175H"
+  };
